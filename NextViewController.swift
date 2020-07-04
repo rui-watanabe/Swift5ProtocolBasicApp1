8bf27f155c6986c440950eb9b3b44c81c25611aa
rui-watanabe <wataru610806@gmail.com>
2020-07-04 17:21:39 +0900

    set UI 2

--- NextViewController.swift
+++ NextViewController.swift
@@ -9,14 +9,23 @@
 import UIKit
 
 class NextViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    
+    @IBAction func plusAction(_ sender: Any) {
+        count += 1
+        label.text = String(count)
+    }
+    
     /*
     // MARK: - Navigation
 
